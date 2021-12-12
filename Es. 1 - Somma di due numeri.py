f3cebe9dd3dd1d80de36d4f9debfe13ddcd7d7d0
Leonardo Essam Dei Rossi <65762081+leonardodeirossi@users.noreply.github.com>
2021-12-12 10:49:45 +0100

    Upload primo esercizio.

---
+++ Es. 1 - Somma di due numeri.py
@@ -0,0 +1,21 @@
+"""
+Esercizio 1 - Somma di due numeri
+
+Descrizione: Dati in input due numeri esegue la somma e restituisce in output il valore.
+Autore: LEONARDO ESSAM DEI ROSSI (github.com/leonardodeirossi)
+"""
+
+# Definizione delle variabili
+numero1 = 0.0
+numero2 = 0.0
+somma = 0.0
+
+# Fase di input
+numero1 = float(input("Inserisci il primo numero: "))
+numero2 = float(input("Inserisci il secondo numero: "))
+
+# Fase di elaborazione
+somma = numero1 + numero2
+
+# Fase di output
+print("La somma dei numeri è: ", somma)
