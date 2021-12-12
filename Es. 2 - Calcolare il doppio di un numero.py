9de83126c571e6e19fcbcacbf05a8f1e1d37dcb6
Leonardo Essam Dei Rossi <65762081+leonardodeirossi@users.noreply.github.com>
2021-12-12 10:52:30 +0100

    Upload secondo esercizio.

---
+++ Es. 2 - Calcolare il doppio di un numero.py
@@ -0,0 +1,19 @@
+"""
+Esercizio 2 - Calcolare il doppio di un numero
+
+Descrizione: Dato in input un numero calcolare e restituire il suo doppio.
+Autore: LEONARDO ESSAM DEI ROSSI (github.com/leonardodeirossi)
+"""
+
+# Definizione delle variabili
+numero = 0.0
+prodotto = 0.0
+
+# Fase di input
+numero = float(input("Inserisci il numero da moltiplicare: "))
+
+# Fase di elaborazione
+prodotto = numero * 2
+
+# Fase di output
+print("Il doppio del numero è: ", prodotto)
